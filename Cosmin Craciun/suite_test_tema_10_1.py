6f6057400d55a3c1a65b343f9c523be669a64a03
Cosmin <Craciun.R.Cosmin@gmail.com>
2022-10-13 14:10:01 +0300

    Tema 10.

---
+++ Cosmin Craciun/suite_test_tema_10_1.py
@@ -0,0 +1,18 @@
+import unittest
+import HTMLTestRunner
+
+from tema_9_obligatorie_verificatori import Internet_Hero
+from tema_10_test_alerts import EdgeTestCase
+
+
+class TestSuit(unittest.TestCase):
+
+    def test_suite(self):
+        tests_to_run = unittest.TestSuite()
+        tests_to_run.addTests([
+            unittest.defaultTestLoader.loadTestsFromTestCase(Internet_Hero),
+            unittest.defaultTestLoader.loadTestsFromTestCase(EdgeTestCase)
+        ])
+
+        runner = HTMLTestRunner.HTMLTestRunner(report_name='First report of test runner', tested_by="Cosmin Craciun")
+        runner.run(tests_to_run)
