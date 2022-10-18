6858c3476f784cf73fa8593e2b65efd322dc2e99
Adrian <adrianamcovei17@gmail.com>
2022-10-18 15:31:55 +0300

    Added inheritance browser class in selenium_tests_javascript_alerts

--- Macovei Adrian/Tema10/Cross browser java script alerts selenium tests/selenium_tests_javascript_alerts_cross_browser.py
+++ Macovei Adrian/Tema10/Cross browser java script alerts selenium tests/selenium_tests_javascript_alerts_cross_browser.py
@@ -110,12 +110,7 @@ class EdgeTestCase(TestCase):
         assert result.text == "You entered: null"
 
 
-class FirefoxTestCase(TestCase):
-    BASE_URL = "https://the-internet.herokuapp.com/javascript_alerts"
-    ALERT_BUTTON_SELECTOR = (By.XPATH, "//ul/li[1]/button")
-    CONFIRM_BUTTON_SELECTOR = (By.XPATH, "//ul/li[2]/button")
-    PROMPT_BUTTON_SELECTOR = (By.XPATH, "//ul/li[3]/button")
-    RESULT_SELECTOR = (By.ID, "result")
+class FirefoxTestCase(EdgeTestCase):
 
     def setUp(self) -> None:
         service = FirefoxService(GeckoDriverManager().install())
@@ -127,92 +122,8 @@ class FirefoxTestCase(TestCase):
     def tearDown(self) -> None:
         self.browser.quit()
 
-    def test_simple_alert(self):
-        alert_button = self.browser.find_element(*self.ALERT_BUTTON_SELECTOR)
-        alert_button.click()
 
-        alert_window = self.browser.switch_to.alert
-        alert_window.accept()
-        time.sleep(1)
-
-        result = self.browser.find_element(*self.RESULT_SELECTOR)
-        assert result.text == "You successfully clicked an alert"
-
-    def test_confirm_alert_accept(self):
-        confirm_button = self.browser.find_element(*self.CONFIRM_BUTTON_SELECTOR)
-        confirm_button.click()
-
-        confirm_window = self.browser.switch_to.alert
-        confirm_window.accept()
-        time.sleep(1)
-
-        result = self.browser.find_element(*self.RESULT_SELECTOR)
-        assert result.text == "You clicked: Ok"
-
-    def test_confirm_alert_dismiss(self):
-        confirm_button = self.browser.find_element(*self.CONFIRM_BUTTON_SELECTOR)
-        confirm_button.click()
-
-        confirm_window = self.browser.switch_to.alert
-        confirm_window.dismiss()
-        time.sleep(1)
-
-        result = self.browser.find_element(*self.RESULT_SELECTOR)
-        assert result.text == "You clicked: Cancel"
-
-    def test_prompt_alert_accept_empty(self):
-        prompt_button = self.browser.find_element(*self.PROMPT_BUTTON_SELECTOR)
-        prompt_button.click()
-
-        prompt_window = self.browser.switch_to.alert
-        prompt_window.accept()
-        time.sleep(1)
-
-        result = self.browser.find_element(*self.RESULT_SELECTOR)
-        assert result.text == "You entered:"
-
-    def test_prompt_alert_accept_with_insert(self):
-        prompt_button = self.browser.find_element(*self.PROMPT_BUTTON_SELECTOR)
-        prompt_button.click()
-
-        prompt_window = self.browser.switch_to.alert
-        prompt_window.send_keys("Salut!")
-        prompt_window.accept()
-        time.sleep(1)
-
-        result = self.browser.find_element(*self.RESULT_SELECTOR)
-        assert result.text == "You entered: Salut!"
-
-    def test_prompt_alert_dismiss_empty(self):
-        prompt_button = self.browser.find_element(*self.PROMPT_BUTTON_SELECTOR)
-        prompt_button.click()
-
-        prompt_window = self.browser.switch_to.alert
-        prompt_window.dismiss()
-        time.sleep(1)
-
-        result = self.browser.find_element(*self.RESULT_SELECTOR)
-        assert result.text == "You entered: null"
-
-    def test_prompt_alert_dismiss_with_insert(self):
-        prompt_button = self.browser.find_element(*self.PROMPT_BUTTON_SELECTOR)
-        prompt_button.click()
-
-        prompt_window = self.browser.switch_to.alert
-        prompt_window.send_keys("Salut!")
-        prompt_window.dismiss()
-        time.sleep(1)
-
-        result = self.browser.find_element(*self.RESULT_SELECTOR)
-        assert result.text == "You entered: null"
-
-
-class ChromeTestCase(TestCase):
-    BASE_URL = "https://the-internet.herokuapp.com/javascript_alerts"
-    ALERT_BUTTON_SELECTOR = (By.XPATH, "//ul/li[1]/button")
-    CONFIRM_BUTTON_SELECTOR = (By.XPATH, "//ul/li[2]/button")
-    PROMPT_BUTTON_SELECTOR = (By.XPATH, "//ul/li[3]/button")
-    RESULT_SELECTOR = (By.ID, "result")
+class ChromeTestCase(EdgeTestCase):
 
     def setUp(self) -> None:
         self.browser = webdriver.Chrome(service=selenium.webdriver.chrome.service.Service(ChromeDriverManager().install()))
@@ -222,82 +133,3 @@ class ChromeTestCase(TestCase):
 
     def tearDown(self) -> None:
         self.browser.quit()
-
-    def test_simple_alert(self):
-        alert_button = self.browser.find_element(*self.ALERT_BUTTON_SELECTOR)
-        alert_button.click()
-
-        alert_window = self.browser.switch_to.alert
-        alert_window.accept()
-        time.sleep(1)
-
-        result = self.browser.find_element(*self.RESULT_SELECTOR)
-        assert result.text == "You successfully clicked an alert"
-
-    def test_confirm_alert_accept(self):
-        confirm_button = self.browser.find_element(*self.CONFIRM_BUTTON_SELECTOR)
-        confirm_button.click()
-
-        confirm_window = self.browser.switch_to.alert
-        confirm_window.accept()
-        time.sleep(1)
-
-        result = self.browser.find_element(*self.RESULT_SELECTOR)
-        assert result.text == "You clicked: Ok"
-
-    def test_confirm_alert_dismiss(self):
-        confirm_button = self.browser.find_element(*self.CONFIRM_BUTTON_SELECTOR)
-        confirm_button.click()
-
-        confirm_window = self.browser.switch_to.alert
-        confirm_window.dismiss()
-        time.sleep(1)
-
-        result = self.browser.find_element(*self.RESULT_SELECTOR)
-        assert result.text == "You clicked: Cancel"
-
-    def test_prompt_alert_accept_empty(self):
-        prompt_button = self.browser.find_element(*self.PROMPT_BUTTON_SELECTOR)
-        prompt_button.click()
-
-        prompt_window = self.browser.switch_to.alert
-        prompt_window.accept()
-        time.sleep(1)
-
-        result = self.browser.find_element(*self.RESULT_SELECTOR)
-        assert result.text == "You entered:"
-
-    def test_prompt_alert_accept_with_insert(self):
-        prompt_button = self.browser.find_element(*self.PROMPT_BUTTON_SELECTOR)
-        prompt_button.click()
-
-        prompt_window = self.browser.switch_to.alert
-        prompt_window.send_keys("Salut!")
-        prompt_window.accept()
-        time.sleep(1)
-
-        result = self.browser.find_element(*self.RESULT_SELECTOR)
-        assert result.text == "You entered: Salut!"
-
-    def test_prompt_alert_dismiss_empty(self):
-        prompt_button = self.browser.find_element(*self.PROMPT_BUTTON_SELECTOR)
-        prompt_button.click()
-
-        prompt_window = self.browser.switch_to.alert
-        prompt_window.dismiss()
-        time.sleep(1)
-
-        result = self.browser.find_element(*self.RESULT_SELECTOR)
-        assert result.text == "You entered: null"
-
-    def test_prompt_alert_dismiss_with_insert(self):
-        prompt_button = self.browser.find_element(*self.PROMPT_BUTTON_SELECTOR)
-        prompt_button.click()
-
-        prompt_window = self.browser.switch_to.alert
-        prompt_window.send_keys("Salut!")
-        prompt_window.dismiss()
-        time.sleep(1)
-
-        result = self.browser.find_element(*self.RESULT_SELECTOR)
-        assert result.text == "You entered: null"
